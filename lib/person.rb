bba236cebc5291a2195f065a719b205c33adf654
likhitaaluru <likhitaaluru@gmail.com>
2018-08-10 16:47:00 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,11 @@
 # person.rb
 class Person
   attr_reader :name
-  attr_accessor :breed
   def name=(name)
     @name = name
   end
 end
 
-fido = Dog.new 
-fido.name = "Fido"
-puts fido.name
+joe = Person.new 
+joe.name = "Joe"
+puts joe.name
